795cda95dd7b7f9645d9728b98034ccbdf639709
Guus Beckett <ghbeckett@gmail.com>
2013-12-09 14:11:09 +0100

    All media classes added

---
+++ Wander/WanderLib/Media/Media.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanderLib.Media
+{
+    class Media
+    {
+        private string fileLocation { get; set; }
+        
+        public Media()
+        {
+            fileLocation = "";
+        }
+    
+    }
+}
