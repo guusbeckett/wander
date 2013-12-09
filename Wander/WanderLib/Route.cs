aaf86dd9a231af9667abe122dedfc598393a0d1f
Guus Beckett <ghbeckett@gmail.com>
2013-12-09 19:20:31 +0100

    Added Route class

--- Wander/WanderLib/Route.cs
+++ Wander/WanderLib/Route.cs
@@ -8,5 +8,27 @@ namespace WanderLib
 {
     class Route
     {
+        public List<Sight> sights { get; set; }
+        public double totalDistance { get; set; }
+        public double distanceLeft { get; set; }
+        public Location lastLocation { get; set; }
+        
+        public Route(List<Sight> sights, double totalDistance)
+        {
+            this.totalDistance = totalDistance;
+            this.sights = sights;
+        }
+
+        public double routeLeft()
+        {
+            return distanceLeft;
+        }
+
+        public double routeWalked()
+        {
+            return totalDistance - distanceLeft;
+        }
+
+
     }
 }
