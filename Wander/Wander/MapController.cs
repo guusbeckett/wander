7221ef09993c4dc7370ab79e2b88771defaac886
= <=>
2013-12-09 15:57:57 +0100

    made stubby MapController class

---
+++ Wander/Wander/MapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wander
+{
+    class MapController
+    {
+        private MapController instance;
+
+        public MapController getInstance()
+        {
+            if (instance == null)
+                instance = new MapController();
+            return instance;
+        }
+    }
+}
