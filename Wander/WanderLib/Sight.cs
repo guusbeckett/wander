43203ba51dac19e662063f25e8de6f0e71a99880
Guus Beckett <ghbeckett@gmail.com>
2013-12-09 19:37:58 +0100

    Added Sight class

--- Wander/WanderLib/Sight.cs
+++ Wander/WanderLib/Sight.cs
@@ -14,6 +14,22 @@ namespace WanderLib
         private Location location { get; set; }
         private Boolean isVisited;
 
+        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location)
+        {
+            this.media = media;
+            this.name = name;
+            this.information = information;
+            this.location = location;
+            isVisited = false;
+        }
+        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location, Boolean isVisited)
+        {
+            this.media = media;
+            this.name = name;
+            this.information = information;
+            this.location = location;
+            this.isVisited = isVisited;
+        }
         private void setToVisited()
         {
             isVisited = true;
