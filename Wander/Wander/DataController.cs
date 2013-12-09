8c7190d3ddafa34e62e1fcb5b4782915ee3e22a7
= <=>
2013-12-09 14:43:42 +0100

    made stubby DataController class

---
+++ Wander/Wander/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wander
+{
+    class DataController
+    {
+        private DataController instance;
+
+        public DataController getInstance()
+        {
+            if (instance == null)
+                instance = new DataController();
+            return instance;
+        }
+    }
+}
