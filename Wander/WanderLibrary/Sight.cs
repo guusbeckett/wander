d2eff4e1387c22f99b1c7481f938bba71e474dc7
Guus Beckett <ghbeckett@gmail.com>
2013-12-12 12:06:34 +0100

    Added waypoint

--- Wander/WanderLibrary/Sight.cs
+++ Wander/WanderLibrary/Sight.cs
@@ -6,15 +6,13 @@ using System.Threading.Tasks;
 
 namespace WanderLib
 {
-    public class Sight
+    public class Sight : Waypoint
     {
         public Dictionary<Media.Media.Type, Media.Media> media { get; set; }
         public string name { get; set; }
-        public string information { get; set; }
-        public Location location { get; set; }
         public Boolean isVisited;
 
-        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location)
+        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location):base(information, location)
         {
             this.media = media;
             this.name = name;
@@ -22,7 +20,7 @@ namespace WanderLib
             this.location = location;
             isVisited = false;
         }
-        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location, Boolean isVisited)
+        public Sight(Dictionary<Media.Media.Type, Media.Media> media, string name, string information, Location location, Boolean isVisited):base(information,location)
         {
             this.media = media;
             this.name = name;
@@ -30,6 +28,7 @@ namespace WanderLib
             this.location = location;
             this.isVisited = isVisited;
         }
+        
         public void setToVisited()
         {
             isVisited = true;
