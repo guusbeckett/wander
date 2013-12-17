624ae5ffdcc37f5560e6f4648a89a3e50401a3b1
Guus Beckett <ghbeckett@gmail.com>
2013-12-17 14:07:36 +0100

    Added waypoint

---
+++ Wander/WanderLibrary/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WanderLib
+{
+    public class Waypoint
+    {
+        public string information { get; set; }
+        public Location location { get; set; }
+
+        public Waypoint(string information, Location location)
+        {
+            this.information = information;
+            this.location = location;
+        }
+
+
+    }
+}
