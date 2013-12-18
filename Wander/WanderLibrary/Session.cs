96a63f8c83d34c4cc5de385fd0ae4730c09fade1
Guus Beckett <ghbeckett@gmail.com>
2013-12-18 15:49:50 +0100

    Added walked route to session

--- Wander/WanderLibrary/Session.cs
+++ Wander/WanderLibrary/Session.cs
@@ -13,7 +13,8 @@ namespace WanderLib
         public Route route { get; set; }
         [global::ProtoBuf.ProtoMember(2, IsRequired = true, Name = @"Settings", DataFormat = global::ProtoBuf.DataFormat.Default)]
         public Settings settings { get; set; }
-        
+        [global::ProtoBuf.ProtoMember(3, IsRequired = true, Name = @"RouteWalked", DataFormat = global::ProtoBuf.DataFormat.Group)]
+        public List<Location> routeWalked { get; set; }
 
         public Session()
         {
